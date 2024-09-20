5ca926118eccd8be440ac69fbe4db13d3c350be9
suresh <saise7028@gmail.com>
2024-09-20 21:10:00 +0530

    terraform

---
+++ expense-1/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_names" {
+  type        = list(string)
+  default     = ["mysql", "backend", "frontend"]
+  description = "description"
+
+}
+
+variable "domain_name" {
+  default = "awsd81s.online"
+
+}
+
+variable "zone_id" {
+  default = "Z01044063630C825L5H0L"
+
+}
